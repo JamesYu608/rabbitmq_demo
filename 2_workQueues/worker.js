b793158dd2b961fea52e673f08af10e9ff4b24d1
jamesyu608 <jamesyu608@gmail.com>
2018-07-17 17:23:38 +0800

    2. Fix comment error

--- 2_workQueues/worker.js
+++ 2_workQueues/worker.js
@@ -1,10 +1,11 @@
-// [Ack]: 當有consumer斷線 (做再久也不算，要真的斷)，RabbitMQ會將沒有ch.ack的messages重送到其它consumer
+// 先前的例子，若producer send message時沒有任何consumer，message會直接不見，如何讓message被處理完才從queue中移除?
+// [Ack]: Consumer呼叫ch.ack才算處理完，當有consumer斷線 (做很久不算)，RabbitMQ會將沒有ch.ack的messages重送到其它consumer
 // Note: Ack預設是true，很常會沒有要用卻忘記加上{noAck: false}
 // 這種情況下當然也不會寫ch.ack，導致messages雖然都有處理但是有consumer離線時又全部重送
 
 // [Durability]: RabbitMQ server斷線 (或crash)，回復queue跟message (不完全保證，完全保證看文件)
 // 1. 確保queue是durable: 記得producer跟consumer都要
-// 2. 確保message是durable: consumer的send
+// 2. 確保message是durable: producer的sendToQueue
 
 // [Prefetch]: 一次處理幾個tasks (在全部ch.ack前不會再拿tasks進來，打破round-robin dispatching)
 
